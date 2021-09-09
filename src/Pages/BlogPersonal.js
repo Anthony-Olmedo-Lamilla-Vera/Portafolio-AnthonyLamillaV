d7db5960ec2779805cf28812b861eaf3c8ffb059
Lamilla19 <57071153+Lamilla19@users.noreply.github.com>
2021-09-09 15:41:57 -0500

    links

--- src/Pages/BlogPersonal.js
+++ src/Pages/BlogPersonal.js
@@ -1,26 +1,36 @@
 import React from "react";
 import CardBlogPersonal from "../Components/CardBlogPersonal";
+import Footer from "../Components/Footer";
 import Section from "../Components/Section";
 import Subtitulos from "../Components/Subtitulos";
 
 function BlogPersonal() {
   return (
-    <div className="Blogpersonal">
-      <div className="width-Proyectos">
-        <div className="cont-blog">
-          <div className="section-blogpersonal">
-            <Section text="Blog Personal" />
-          </div>
-          <Subtitulos text="Publicaciones" />
-          <div className="Card-BlogPersonal">
-            <CardBlogPersonal />
-            <CardBlogPersonal />
-            <CardBlogPersonal />
-            <CardBlogPersonal />
+    <>
+      <main>
+        <div className="Blogpersonal">
+          <div className="width-Proyectos">
+            <div className="cont-blog">
+              <div className="section-blogpersonal">
+                <Section text="Blog Personal" />
+              </div>
+              <div className="subtitulo-blog">
+                <Subtitulos text="Publicaciones" />
+              </div>
+              <div className="Card-BlogPersonal">
+                <CardBlogPersonal />
+                <CardBlogPersonal />
+                <CardBlogPersonal />
+                <CardBlogPersonal />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
 
--- src/Pages/Proyectos.js
+++ src/Pages/Proyectos.js
@@ -1,21 +1,56 @@
 import React from "react";
 import Card from "../Components/Card";
+import Footer from "../Components/Footer";
 import Section from "../Components/Section";
+import AppNotas from "../IMG/Appnotas.png";
+import Store from "../IMG/Store.png";
 
 function Proyectos() {
+  const descripcion = {
+    primer: [
+      "libreria React",
+      "Crud de Datos",
+      "Backend con Node ",
+      "Pagos Mediante Paypal",
+      "Deploy GitHub Pages",
+    ],
+    segundo: [
+      "Crud Mongo Db ",
+      "Peticiones Http ",
+      "Api Rest ",
+      "Api Rest Full ",
+    ],
+  };
   return (
-    <div className="Proyectos">
-      <div className="width-Proyectos">
-        <div className="cont-Proyectos">
-          <div className="Titte-Section">
-            <Section text="Proyectos" />
-          </div>
-          <div className="item-proyectos">
-            <Card />
+    <>
+      <main>
+        <div className="Proyectos">
+          <div className="width-Proyectos">
+            <div className="cont-Proyectos">
+              <div className="Titte-Section">
+                <Section text="Proyectos" />
+              </div>
+              <div className="item-proyectos">
+                <Card
+                  img={Store}
+                  descrip1={descripcion.primer}
+                  link="https://antocraxx.github.io/StoreMuebles/"
+                  repo="https://github.com/Antocraxx/StoreMuebles"
+                />
+                <Card
+                  img={AppNotas}
+                  descrip1={descripcion.segundo}
+                  link="https://antocraxx.github.io/AplicacionWebNotas/"
+                  repo="https://github.com/Antocraxx/AplicacionWebNotas"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </main>
+      <footer></footer>
+      <Footer />
+    </>
   );
 }
 
